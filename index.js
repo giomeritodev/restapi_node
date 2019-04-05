e149a1aaf95102bb72a05c191ac40349ec9dfabd
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2019-04-04 22:28:30 -0300

    Atualizando projeto

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 4000;
 
 let app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(expressValidator());
 
 consign().include('routes').include('utils').into(app);
